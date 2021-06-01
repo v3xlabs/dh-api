f68b07502175bbc6781642370212e188eddcf37d
Luc <luc@lucemans.nl>
2021-06-01 22:48:53 +0200

    FIXED

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -2,6 +2,8 @@ import { Args, ArgsType, Ctx, Field, FieldResolver, Int, Mutation, Query, Resolv
 import { User } from "../types/user";
 import { UserContext } from '../graphql-utils/pullUserFromRequest';
 import { Room } from "../types/room";
+import { getManager, getRepository } from "typeorm";
+import { Follow } from "../types/follow";
 
 @ArgsType()
 class FollowUserArgs {
@@ -18,14 +20,15 @@ class GetUserArgs {
 @Resolver(of => User)
 export class UserResolver {
 
-    @Query(returns => User)
-    async me(@Ctx() ctx: UserContext) {
-        return User.findOne({where: {id: ctx.user_id}});
-    }
+    // @Query(returns => User)
+    // async me(@Ctx() ctx: UserContext) {
+    //     return User.findOne({where: {id: ctx.user_id}});
+    // }
 
     @Query(returns => User)
     async user(@Args() {user_id}: GetUserArgs) {
-        return User.findOne({where: {id: user_id}});
+        const userRepository = getRepository(User);
+        return userRepository.findOne({where: {id: user_id}, relations: ["following", "followers"]});
     }
 
     @FieldResolver()
@@ -39,58 +42,66 @@ export class UserResolver {
         });
     }
 
-    @FieldResolver()
-    async follower_count(@Root() user: User): Promise<number> {
-        return (await user.followers).length;
-    }
+    // @FieldResolver()
+    // async follower_count(@Root() user: User): Promise<number> {
+    //     return (await user.followers).length;
+    // }
+
+    // @FieldResolver()
+    // async following_count(@Root() user: User): Promise<number> {
+    //     return (await user.following).length;
+    // }
 
     @FieldResolver()
-    async following_count(@Root() user: User): Promise<number> {
-        return (await user.following).length;
+    async following(@Root() user: User): Promise<Follow[]> {
+        const m = getManager();
+        const f = await m.find(Follow, {where: {follower: user}, relations: ['follower', 'following']});
+        return f;
+    }
+    
+    @FieldResolver()
+    async followers(@Root() user: User): Promise<Follow[]> {
+        const m = getManager();
+        const f = await m.find(Follow, {where: {following: user}, relations: ['follower', 'following']});
+        return f;
     }
 
     @Mutation(type => Boolean)
     async follow(@Ctx() ctx: UserContext, @Args() {user_id}: FollowUserArgs) {
-        const user = await User.findOne({where: {id: ctx.user_id}, relations: ['following']});
-        const otherUser = await User.findOne({where: {id: user_id}});
+        const m = getManager();
 
-        if (!otherUser) {
-            return false;
-        }
+        const user = await m.findOne(User, {where: {id: ctx.user_id}});
+        const otherUser = await m.findOne(User, {where: {id: user_id}});
 
-        const following = await user.following;
+        console.log(user, otherUser);
 
-        if (following.some(following => following.id == otherUser.id)) {
-            return true;
-        }
-
-        following.push(otherUser);
-        user.following = following;
-
-        await user.save();
+        const f = m.create(Follow, {});
+        f.follower = user;
+        f.following = otherUser;
+        await m.save(f);
 
         return true;
     }
 
-    @Mutation(type => Boolean)
-    async unfollow(@Ctx() ctx: UserContext, @Args() {user_id}: FollowUserArgs) {
-        const user = await User.findOne({where: {id: ctx.user_id}, relations: ['following']});
-        const otherUser = await User.findOne({where: {id: user_id}});
+    // @Mutation(type => Boolean)
+    // async unfollow(@Ctx() ctx: UserContext, @Args() {user_id}: FollowUserArgs) {
+    //     const user = await User.findOne({where: {id: ctx.user_id}, relations: ['following']});
+    //     const otherUser = await User.findOne({where: {id: user_id}});
 
-        if (!otherUser) {
-            return false;
-        }
+    //     if (!otherUser) {
+    //         return false;
+    //     }
 
-        const following = await user.following;
+    //     const following = await user.following;
 
-        if (!following.some(following => following.id == otherUser.id)) {
-            return true;
-        }
+    //     if (!following.some(following => following.id == otherUser.id)) {
+    //         return true;
+    //     }
         
-        user.following = following.filter(following => following.id !== otherUser.id);
+    //     // user.following = following.filter(following => following.id !== otherUser.id);
 
-        await user.save();
+    //     await user.save();
 
-        return true;
-    }
+    //     return true;
+    // }
 }
