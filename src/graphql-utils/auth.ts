eb49c8141e1c340efc01466667d3401594f23611
Lucemans <luc@lucemans.nl>
2021-06-07 22:40:21 +0200

    Throw Error On Non Auth (#48)

--- src/graphql-utils/auth.ts
+++ src/graphql-utils/auth.ts
@@ -4,7 +4,7 @@ import { UserContext } from "./pullUserFromRequest";
 export const useAuth: MiddlewareFn<UserContext> = ({args, context, info, root}, next) => {
     if (!context.user_id) {
         console.log('Unauthorized Request to User Endpoint');
-        return null;
+        throw new Error('Unauthorized');
     }
     return next();
 };
