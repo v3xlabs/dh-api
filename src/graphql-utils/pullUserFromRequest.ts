221d2646dd9940546bc08f69ed9c4b77c356ae75
Lucemans <luc@lucemans.nl>
2021-06-07 22:32:09 +0200

    Reworked Auth to support Generation (#47)
    Fixes

--- src/graphql-utils/pullUserFromRequest.ts
+++ src/graphql-utils/pullUserFromRequest.ts
@@ -5,25 +5,20 @@ export type UserContext = {
   user_id: number;
 };
 
-export const pullUserFromRequest = (a: {[key: string]: string | string[]}) => {
-  try {
-    const token = a.authorization;
+export const pullUserFromRequest = (a: { [key: string]: string | string[] }) => {
+  const token = a.authorization;
 
-    if (!token) {
-      return null;
-    }
-
-    if (typeof token !== 'string') {
-      return null;
-    }
-
-    if (!verify(token.replace("Bearer ", ""), process.env.AUTH_TOKEN)) {
-      return null;
-    }
-
-    return decode(token.replace("Bearer ", ""))["id"];
-  } catch (error) {
-    console.log(error);
+  if (!token) {
     return null;
   }
+
+  if (typeof token !== 'string') {
+    return null;
+  }
+
+  if (!verify(token.replace("Bearer ", ""), process.env.AUTH_TOKEN)) {
+    return null;
+  }
+
+  return decode(token.replace("Bearer ", ""))["id"];
 }
